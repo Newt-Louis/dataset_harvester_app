3485ccf581e692fcb404746d64a419114dd1bb2f
Newt Louis <l.ngoc.17395@gmail.com>
2026-03-04 22:35:44 +0700

    add a dominant color range

--- src/main.js
+++ src/main.js
@@ -1,12 +1,36 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+
 import PrimeVue from 'primevue/config';
 import Aura from '@primevue/themes/aura';
+import { definePreset } from '@primevue/themes';
+import 'primeicons/primeicons.css';
+
+const AIPreset = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: '#ecfeff',
+            100: '#cffafe',
+            200: '#a5f3fc',
+            300: '#67e8f9',
+            400: '#22d3ee',
+            500: '#00d4ff',
+            600: '#0891b2',
+            700: '#0e7490',
+            800: '#155e75',
+            900: '#164e63',
+            950: '#083344'
+        }
+    }
+});
 
 const app = createApp(App);
 app.use(PrimeVue,{
     theme:{
-        preset: Aura
+        preset: AIPreset,
+        options:{
+            darkModeSelector: ".app-dark",
+        }
     }
 });
 
