3c709f224d1a2089ed30bed246366eb10a3a60b3
Newt Louis <l.ngoc.17395@gmail.com>
2026-04-06 01:07:14 +0700

    fix error varialble name

--- src/api/client.js
+++ src/api/client.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000'
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000'
 
 export const api = axios.create({
   baseURL: BASE_URL,
